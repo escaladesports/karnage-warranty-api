b7760af5d36977ec7f46f6e8a81fbb20cd6b4ece
Christian Sedlacek <csedlacek@escaladesports.com>
2017-09-20 09:19:04 -0500

    Updated handler and serverless config files to handle API requests to cpsia/{brand}/{business} endpoint

--- handler.js
+++ handler.js
@@ -1,16 +1,40 @@
 'use strict';
 
-module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+const dealerApi = require('./src/dealer-api.js');
+
+module.exports.endpoint = (event, context, callback) => {
+  const params = {
+    brand: event.pathParameters.brand,
+    business: event.pathParameters.business
   };
 
-  callback(null, response);
+  cpsiaApi.cpsiaRequest(params).then(cpsiaData => {
+    const response = {
+      statusCode: 200,
+      body: cpsiaData,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true
+      }
+    };
+    callback(null, response);
+  }).catch(err => {
+    let msg = 'There was a problem with your request. Please try again later';
+    
+    if (err.code === 'malformed') {
+      msg = 'The request data was malformed- ensure parameter data is correct and try again.'
+    }
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    const response = {
+      statusCode: 400,
+      body: JSON.stringify({
+        errors: [msg]
+      }),
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Credentials": true
+      }
+    };
+    callback(null, response);
+  });
 };
