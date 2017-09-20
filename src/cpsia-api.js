c5e0cc23d9ac621391759bb3aeb047ef8a1538af
Christian Sedlacek <csedlacek@escaladesports.com>
2017-09-20 09:18:07 -0500

    Added main CPSIA API request code

---
+++ src/cpsia-api.js
@@ -0,0 +1,30 @@
+const request = require('request-promise-native');
+
+function proximityRequest(brand, business) {
+	const endpoint = `https://apis.escaladesports.com/v1/cpsia/get/${brand}/${business}`;
+	return request(endpoint);
+}
+
+
+
+/**
+* Use to make a request to the Escalade CPSIA Certificate API (if passed valid data)
+* @param {Object} data Request data
+* @param {String} data.brand Brand name to retrieve CPSIA certificates for
+* @param {Number} data.business Business ('sports' or 'archery') to retrieve CPSIA certificates for
+* @returns {Promise.<Object>} Returns a promise resolving to a JSON response from the API (or an error
+* if passed invalid data)
+* @example
+* cpsiaApi.cpsiaRequest('bear', 'archery').then( ... )
+*/
+module.exports.cpsiaRequest = function(data) {
+	// validate
+	if (!data.brand || !data.business) {
+		return Promise.reject({
+			code: 'malformed',
+			error: new Error('Malformed request data')
+		});
+	}
+	// make request if valid
+	return cpsiaRequest(data.brand, data.business);
+}
