96577910b24cfd8127610cda805561b6791dd508
Christian Sedlacek <csedlacek@escaladesports.com>
2017-10-12 16:06:30 -0500

    Restructured formatManualsResponse function to work with the manuals API response structure

--- src/manuals-api.js
+++ src/manuals-api.js
@@ -14,21 +14,30 @@ function manualsRequest(brand) {
 */
 function formatManualsResponse(responseData) {
 	const parsed = typeValidator.isString(responseData) ? JSON.parse(responseData) : responseData;
-	const fileData = parsed.data.files;
+	const fileData = parsed.files;
 	const formattedData = {
-		certificates: []
+		manuals: []
 	};
-	// Escalade API gives files as an object with directory names as keys and an array of
-	// file names under that directory as values, but we want to flatten this structure out
-	// into a single array of file info objects
+
+	// response is formatted as
+	// { 
+	//		files: {
+	//			'fileDirName': {  // ex '/var/www/html/manual-files/goalrilla/accessories'
+	//				'fileTitle': 'file_name.pdf'  // ex 'B2412' : 'B2412_2L679801'
+	//			}
+	// 		}
+	// }
 	for (let dir in fileData) {
 		const files = fileData[dir];
-		const formattedFiles = files.map(file => ({
-			file,
-			name: file.replace(/\.[^/.]+$/, ""),
-			directory: dir
-		}));
-		formattedData.certificates.push(...formattedFiles);
+		for (let fileName in files) {
+			const file = files[fileName];
+			const formattedFile = {
+				file,
+				name: fileName,
+				dir: dir
+			}
+			formattedData.manuals.push(formattedFile);
+		}
 	}
 	return formattedData;
 }
@@ -38,7 +47,7 @@ function formatManualsResponse(responseData) {
 /**
 * Use to make a request to the Escalade manuals API (if passed valid data)
 * @param {Object} data Request data
-* @param {String} data.brand Brand name to retrieve manuals certificates for
+* @param {String} data.brand Brand name to retrieve manuals for
 * @returns {Promise.<Object>} Returns a promise resolving to a JSON response from the API (or an error
 * if passed invalid data)
 * @example
