02f3000e25dc9db1fd67b3acc767a5a7bf716f06
Christian Sedlacek <csedlacek@escaladesports.com>
2017-10-12 12:29:51 -0500

    Renamed manuals-api functions and variables to use 'manuals' naming instead of 'CPSIA'

--- src/manuals-api.js
+++ src/manuals-api.js
@@ -1,18 +1,18 @@
 const request = require('request-promise-native');
 const typeValidator = require('./typeValidator.js');
 
-function cpsiaRequest(brand, business) {
-	const endpoint = `https://apis.escaladesports.com/v1/cpsia/get/${brand}/${business}`;
+function manualsRequest(brand, business) {
+	const endpoint = `https://apis.escaladesports.com/v1/manuals/get/${brand}/${business}`;
 	return request(endpoint);
 }
 
 /**
-* Formats raw responses from the Escalade CPSIA Certificate API into a more easily consumable form
+* Formats raw responses from the Escalade manuals Certificate API into a more easily consumable form
 * @param {Object} responseData API response data
 * @param {Object.<Object>} responseData.files Object containing various arrays of certificate file names
 * @returns {Object} Returns an object containing formatted response data
 */
-function formatCpsiaResponse(responseData) {
+function formatManualsResponse(responseData) {
 	const parsed = typeValidator.isString(responseData) ? JSON.parse(responseData) : responseData;
 	const fileData = parsed.data.files;
 	const formattedData = {
@@ -36,16 +36,16 @@ function formatCpsiaResponse(responseData) {
 
 
 /**
-* Use to make a request to the Escalade CPSIA Certificate API (if passed valid data)
+* Use to make a request to the Escalade manuals Certificate API (if passed valid data)
 * @param {Object} data Request data
-* @param {String} data.brand Brand name to retrieve CPSIA certificates for
-* @param {Number} data.business Business ('sports' or 'archery') to retrieve CPSIA certificates for
+* @param {String} data.brand Brand name to retrieve manuals certificates for
+* @param {Number} data.business Business ('sports' or 'archery') to retrieve manuals certificates for
 * @returns {Promise.<Object>} Returns a promise resolving to a JSON response from the API (or an error
 * if passed invalid data)
 * @example
-* cpsiaApi.cpsiaRequest('bear', 'archery').then( ... )
+* manualsApi.manualsRequest('bear', 'archery').then( ... )
 */
-module.exports.cpsiaRequest = function(data) {
+module.exports.manualsRequest = function(data) {
 	// validate
 	if (!data.brand || !data.business) {
 		return Promise.reject({
@@ -54,5 +54,5 @@ module.exports.cpsiaRequest = function(data) {
 		});
 	}
 	// make request if valid
-	return cpsiaRequest(data.brand, data.business).then(responseData => formatCpsiaResponse(responseData));
+	return manualsRequest(data.brand, data.business).then(responseData => formatManualsResponse(responseData));
 }
