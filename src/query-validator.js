f0eee4984faf835b61bfa684c3e4c10f3dc12aeb
Christian Sedlacek <csedlacek@escaladesports.com>
2017-10-19 16:35:34 -0500

    Updated queryValidator to check for missing keys on request

--- src/query-validator.js
+++ src/query-validator.js
@@ -1,11 +1,32 @@
+function checkMissingKeys(keys, data) {
+	for (let key of keys) {
+		if (!data[key]) {
+			console.log('missing key '+key);
+			return false;
+		}
+	}
+	return true;
+}
 function validateQuotePost(params) {
+	console.log('validating data:');
+	console.dir(params);
 	// check for missing data
-	if (
-		!params.dealerId || !params.dealerName || !params.dealerAddress || !params.dealerCity
-		|| !params.dealerState || !params.dealerZip || !params.dealerPhone || !params.userFirstName
-		|| !params.userLastName || !params.userEmail || !params.userPhone || !params.contactPreference
-		|| !params.userState || !params.userZip
-	) {
+	if (!checkMissingKeys([
+			'dealerId',
+			'dealerName',
+			'dealerAddress',
+			'dealerCity',
+			'dealerState',
+			'dealerZip',
+			'dealerPhone',
+			'userFirstName',
+			'userLastName',
+			'userEmail',
+			'userPhone',
+			'contactPreference',
+			'userState',
+			'userZip'
+		], params)) {
 		return false;
 	}
 	return true;
