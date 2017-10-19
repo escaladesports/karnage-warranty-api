40052e00dff3e08224c5d050a5ba8de136283b51
Christian Sedlacek <csedlacek@escaladesports.com>
2017-10-19 16:48:53 -0500

    Added barebones email module with to-do documentation

---
+++ src/email.js
@@ -0,0 +1,19 @@
+require('dotenv').config({silent: true});
+const sparkpost = require('sparkpost');
+
+const sparkpostKey = process.env.SPARKPOST_API_KEY;
+
+/**
+	Sends a quote request email filled-out with param data to specified recipients
+	@param {Object} data Quote request data from form/etc.
+	@param {String|Array.<String>} sendTo String or array of strings with email addresses of recipients
+	@returns {Promise}
+*/
+function sendQuoteRequestEmail(data, sendTo) {
+// take data and send to sendTo
+	return Promise.resolve('true');
+}
+
+module.exports = {
+	sendQuoteRequestEmail
+}
