7bf9e4254c184511be1ff9717a94d56b991b3565
Christian Sedlacek <csedlacek@escaladesports.com>
2017-10-20 14:30:10 -0500

    Added module for parsing/handling Google Sheets ranges

---
+++ src/range.js
@@ -0,0 +1,12 @@
+/**
+	Determines the final row in a Google Sheets range
+	@param {String} range Range to parse
+	@returns {Number} Final row in range
+*/
+function getFinalRangeRow(range) {
+	return 0;
+}
+
+module.exports = {
+	getFinalRangeRow
+}
