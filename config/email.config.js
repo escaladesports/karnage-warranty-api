05a5f2a380d30d1a4e89f9c0ad03b4b479bdc558
Christian Sedlacek <csedlacek@escaladesports.com>
2017-10-20 09:48:50 -0500

    Added email config file

---
+++ config/email.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	quoteRequestRecipients: [
+		{email: 'christian.l.sedlacek@gmail.com', name: 'Christian Sedlacek'},
+		{email: 'csedlacek@escaladesports.com', name: 'Christian Sedlacek'}
+	]
+}
