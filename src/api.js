8d5782376ea2065c81424ac81d66bf0f11dd3bef
Christian Sedlacek <csedlacek@escaladesports.com>
2018-01-05 11:50:49 -0600

    Disable post-storage email notification

--- src/api.js
+++ src/api.js
@@ -14,7 +14,8 @@ function postWarrantyRegistrationActions(data) {
 		const requestId = range.getFinalRangeRow(res.updates.updatedRange);
 		const updatedData = Object.assign({}, data, { requestId });
 		// email relevant parties
-		return email.sendWarrantyRegistrationEmail(updatedData, emailConfig.warrantyRegistrationRecipients)
+		//return email.sendWarrantyRegistrationEmail(updatedData, emailConfig.warrantyRegistrationRecipients)
+		return updatedData; // emailing disabled, just sent the raw data...
 	});
 }
 
